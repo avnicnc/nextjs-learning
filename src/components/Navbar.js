71a48b540d8b8eb4f1638b0d14534ccda6dbb13b
avniash <avniashar5@gmail.com>
2026-08-18 10:41:20 +0530

    add navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,8 +8,9 @@ export default function Navbar() {
         MyPortfolio
       </Link>
       <div className={styles.navLinks}>
-        <Link href="/" className={styles.link}>Home</Link>
-        <Link href="/about" className={styles.link}>About</Link>
+        <Link href="/" className={styles.link}></Link>
+                <Link href="/about" className={styles.link}>About</Link>
+        <Link href="/contact" className={styles.link}>Contact</Link>
         <Link href="/projects" className={styles.link}>Projects</Link>
       </div>
     </nav>
